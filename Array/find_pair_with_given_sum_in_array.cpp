e77d7f0971a68617647cddb3cbf984d28c35875a
Sahil <sahilatahar2002@gmail.com>
2022-12-10 06:18:27 +0530

    first commit

---
+++ Array/find_pair_with_given_sum_in_array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int num[10] = {1, 2, 5, 3, 6, 7, 8, 9, 6, 2};
+
+    getch();
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# DSA-Day-to-Day
