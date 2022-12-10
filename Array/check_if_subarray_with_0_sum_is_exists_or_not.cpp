91a55d11bc08c2b999f6c9bf84524219c6e3b403
Sahil <sahilatahar2002@gmail.com>
2022-12-10 06:44:01 +0530

    check_if_subarray_with_0_sum_is_exists_or_not completed

---
+++ Array/check_if_subarray_with_0_sum_is_exists_or_not.cpp
@@ -0,0 +1,52 @@
+/*
+Input:  { 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 }
+
+Output: Subarray with zero-sum exists
+
+The subarrays with a sum of 0 are:
+
+{ 3, 4, -7 }
+{ 4, -7, 3 }
+{ -7, 3, 1, 3 }
+{ 3, 1, -4 }
+{ 3, 1, 3, 1, -4, -2, -2 }
+{ 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 }
+*/
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int num[10] = {3, 4, -7, 3, 1, 3, 1, -4, -2, -2};
+    int sum = 0;
+
+    for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+    {
+        sum = num[i];
+
+        for (int j = i + 1; j < sizeof(num) / sizeof(int); j++)
+        {
+            sum += num[j];
+            if (sum == 0)
+            {
+                for (int k = i; k <= j; k++)
+                {
+                    if (k == i)
+                    {
+                        cout << "{";
+                    }
+                    cout << num[k] << ",";
+                    if (k == j)
+                    {
+                        cout << "}\t";
+                    }
+                }
+            }
+        }
+    }
+    // getch();
+    return 0;
+}
