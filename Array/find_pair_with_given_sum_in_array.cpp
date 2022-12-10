33b773498195f8aa74632204df50f0f8657d4a6c
Sahil <sahilatahar2002@gmail.com>
2022-12-10 06:30:47 +0530

    find_pair_with_given_sum_in_array completed

--- Array/find_pair_with_given_sum_in_array.cpp
+++ Array/find_pair_with_given_sum_in_array.cpp
@@ -5,8 +5,26 @@ using namespace std;
 
 int main()
 {
-    int num[10] = {1, 2, 5, 3, 6, 7, 8, 9, 6, 2};
+    int num[10] = {1, 2, 5, 3, 6, 7, 8, 9, 6, 3};
+    int target = 180;
+    bool found = false;
 
+    for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+    {
+        for (int j = i + 1; j < sizeof(num) / sizeof(int); j++)
+        {
+            if (num[i] + num[j] == target)
+            {
+                found = true;
+                cout << "(" << num[i] << "," << num[j] << ")\t";
+            }
+        }
+    }
+
+    if (!found)
+    {
+        cout << "No pair found";
+    }
     getch();
     return 0;
 }
